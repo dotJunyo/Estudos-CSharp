44425f8c28f0e678fbfaf827996216160846dc1a
Junyo <44989305+JunyoTyber@users.noreply.github.com>
2021-12-01 16:18:23 -0300

    Primeiros projetos desenvolvidos
    3 primeiros projetos feitos seguindo o curso de Fundamentos do C#

---
+++ Fundamentos C#/ConsoleApp/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int optMenu = 9;
+
+            while (optMenu != 0)
+            {
+                Console.Clear();
+
+                Console.WriteLine("O que deseja fazer?");
+                Console.WriteLine("1 - Soma");
+                Console.WriteLine("2 - Subtracao");
+                Console.WriteLine("3 - Divisao");
+                Console.WriteLine("4 - Multiplicacao");
+                Console.WriteLine("0 - Sair");
+                Console.WriteLine("");
+
+                optMenu = int.Parse(Console.ReadLine());
+
+                switch (optMenu)
+                {
+                    case 1:
+                        Soma();
+                        break;
+                    case 2:
+                        Subtracao();
+                        break;
+                    case 3:
+                        Divisao();
+                        break;
+                    case 4:
+                        Multiplicacao();
+                        break;
+                }
+            }
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDigite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 + v2;
+
+            Console.WriteLine($"\nO resultado de {v1}+{v2} é: {resultado}\n");
+            Console.ReadKey();
+        }
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDigite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 - v2;
+
+            Console.WriteLine($"\nO resultado de {v1}-{v2} é: {resultado}\n");
+            Console.ReadKey();
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDigite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 / v2;
+
+            Console.WriteLine($"\nO resultado de {v1}/{v2} é: {resultado}\n");
+            Console.ReadKey();
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDigite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 * v2;
+
+            Console.WriteLine($"\nO resultado de {v1}*{v2} é: {resultado}\n");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Fundamentos C#/StopwatchApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace StopwatchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("---Stopwatch---");
+            Console.WriteLine("---------------");
+            Console.WriteLine("M => Minutos | S => Segundos");
+            Console.WriteLine("\nQuanto tempo deseja contar?\n");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            Start(time * multiplier);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime < time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Timer finalizado!");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
